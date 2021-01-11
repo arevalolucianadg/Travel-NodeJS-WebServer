19b06eeaa3bb051a2491fbd7639fea8ecc60ed87
Luciana <arevalolucianadg@gmail.com>
2021-01-11 17:12:39 -0300

    Agrego navbar a web

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const hbs = require('hbs');
 
+app.use( express.static(__dirname + '/public') );
+
 // HandleBars HBS Template engine
 hbs.registerPartials( __dirname + '/views/partials' );
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {
+        year: new Date().getFullYear(),
+    });
 });
 
 app.listen(8080);
